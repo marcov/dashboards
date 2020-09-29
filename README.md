f506cb35a1231e26f2c2d17e0af0d7be3d89d297
Marco Vedovati <mv@sba.lat>
2020-09-29 19:49:47 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+## Grafana
+See `run.sh`.
+
+### GCP
+Partition containig this repo may not be executable, hence run it with:
+```
+$ sudo sh ./run.sh
+```
+
+Auto startup: add the metadata key `startup-script` with this content:
+```
+#!/bin/sh
+/bin/sh /home/USERNAME/monitoring/run.sh
+```
+
+## InfluxDB
+Just a quick test:
+```
+$ docker run --rm -it \
+	--name influxdb \
+	-p 8086:8086 -p 8888:8888 \
+	influxdb
+
+# New telegraf config
+$ docker run --rm telegraf telegraf config > telegraf.conf
+
+$ docker run --rm -it --name=telegraf --net=container:influxdb \
+	-v $PWD/telegraf.conf:/etc/telegraf/telegraf.conf:ro \
+	-v /:/hostfs:ro \
+	-e HOST_ETC=/hostfs/etc \
+	-e HOST_PROC=/hostfs/proc \
+	-e HOST_SYS=/hostfs/sys \
+	-e HOST_VAR=/hostfs/var \
+	-e HOST_RUN=/hostfs/run \
+	-e HOST_MOUNT_PREFIX=/hostfs \
+	telegraf
+
+$ docker run -it --rm \
+	--net=container:influxdb \
+	chronograf --influxdb-url=http://127.0.0.1:8086
+```
---
+++ run.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+#
+#
+#
+set -euo pipefail
+
+declare -r scriptStarted="/tmp/run-sh-started"
+declare -r scriptCompleted="/tmp/run-sh-completed"
+
+rm -f "$scriptCompleted"
+touch "$scriptStarted"
+
+declare -r scriptPath="$(readlink -e $0)"
+declare -r scriptDir="$(dirname "${scriptPath}")"
+
+set -x
+
+docker run \
+	--rm -d \
+	-v "${scriptDir}"/nginx-default.conf:/etc/nginx/conf.d/default.conf \
+        -v "${scriptDir}"/html:/usr/share/nginx/html \
+	--publish 80:80 --publish 3000:3000 --publish 9090:9090 \
+	--name=nginx \
+	nginx \
+
+docker run --rm -d \
+        -v grafana-storage:/var/lib/grafana -v "${scriptDir}"/grafana.ini:/etc/grafana/grafana.ini \
+	--network=container:nginx \
+	--name=grafana \
+	grafana/grafana \
+
+docker run --rm -d \
+        -v prometheus:/prometheus -v "${scriptDir}"/prometheus.yml:/etc/prometheus/prometheus.yml \
+	--network=container:nginx \
+	--name=prom \
+	prom/prometheus \
+
+docker run \
+	--rm -d \
+	-v "${scriptDir}"/fiobbio-meteo.yml:/config.yml \
+	--network=container:nginx \
+	--name=json_fiobbio \
+	quay.io/prometheuscommunity/json-exporter \
+	\
+	--port 7979 http://meteo.fiobbio.com/realtime.json /config.yml \
+
+docker run \
+	--rm -d \
+	-v "${scriptDir}"/fiobbio-meteo.yml:/config.yml \
+	--network=container:nginx \
+	--name=json_misma \
+	quay.io/prometheuscommunity/json-exporter \
+	\
+	--port 7980 http://meteo.fiobbio.com/misma/realtime.json /config.yml \
+
+docker run \
+	--rm -d \
+	-v "${scriptDir}"/home-cancel.yml:/config.yml \
+	--network=container:nginx \
+	--name=json_home \
+	quay.io/prometheuscommunity/json-exporter \
+	\
+	--port 7981 http://jak.sba.lat/cancel/jsonData /config.yml \
+
+docker run \
+	--rm -d \
+  	-v "/:/host:ro,rslave" \
+  	--pid=host \
+	--network=container:nginx \
+	--name=node_exporter \
+	quay.io/prometheus/node-exporter \
+	\
+	--path.rootfs=/host
+
+touch "$scriptCompleted"
+exit 0
