fc2c33a01200eb81572dcd3a851c8910f860af66
sba lat <so@sba.lat>
2020-10-03 07:37:28 +0000

    updates

--- run.sh
+++ run.sh
@@ -13,7 +13,11 @@ touch "$scriptStarted"
 declare -r scriptPath="$(readlink -e $0)"
 declare -r scriptDir="$(dirname "${scriptPath}")"
 
+declare  -r realtimeJsonConfig="realtime-json.yml"
 set -x
+
+# Stop everything
+docker stop `docker ps -a -q` 2>/dev/null || { echo "No containers to stop"; }
 
 docker run \
 	--rm -d \
@@ -34,10 +38,14 @@ docker run --rm -d \
 	--network=container:nginx \
 	--name=prom \
 	prom/prometheus \
+	\
+	--config.file=/etc/prometheus/prometheus.yml \
+	--storage.tsdb.path=/prometheus \
+	--web.enable-admin-api
 
 docker run \
 	--rm -d \
-	-v "${scriptDir}"/fiobbio-meteo.yml:/config.yml \
+	-v "${scriptDir}/${realtimeJsonConfig}":/config.yml \
 	--network=container:nginx \
 	--name=json_fiobbio \
 	quay.io/prometheuscommunity/json-exporter \
@@ -46,12 +54,21 @@ docker run \
 
 docker run \
 	--rm -d \
-	-v "${scriptDir}"/fiobbio-meteo.yml:/config.yml \
+	-v "${scriptDir}/${realtimeJsonConfig}":/config.yml \
 	--network=container:nginx \
 	--name=json_misma \
 	quay.io/prometheuscommunity/json-exporter \
 	\
 	--port 7980 http://meteo.fiobbio.com/misma/realtime.json /config.yml \
+
+docker run \
+	--rm -d \
+	-v "${scriptDir}/${realtimeJsonConfig}":/config.yml \
+	--network=container:nginx \
+	--name=json_villa \
+	quay.io/prometheuscommunity/json-exporter \
+	\
+	--port 7990 http://villameteo.fiobbio.com/weather/realtime.json /config.yml \
 
 docker run \
 	--rm -d \
