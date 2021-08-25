00701be6597997bf75e276ba7df2194fb47e323e
sba lat <so@sba.lat>
2021-08-25 16:20:49 +0200

    ups

--- run.sh
+++ run.sh
@@ -23,23 +23,35 @@ docker run \
 	--rm -d \
 	-v "${scriptDir}"/nginx-default.conf:/etc/nginx/conf.d/default.conf \
         -v "${scriptDir}"/html:/usr/share/nginx/html \
-	--publish 80:80 --publish 3000:3000 --publish 9090:9090 \
+	--publish 80:80 \
+	--publish 3000:3000 \
+	 --publish 9090:9090 \
 	--name=nginx \
 	nginx \
 
-docker run --rm -d \
-        -v grafana-storage:/var/lib/grafana \
-	--network=container:nginx \
-	--env ENABLE_METRICS="true" \
-	--name=grafana-image-renderer \
-	grafana/grafana-image-renderer:latest \
+#docker run --rm -d \
+#        -v grafana-storage:/var/lib/grafana \
+#	--network=container:nginx \
+#	--env ENABLE_METRICS="true" \
+#	--name=grafana-image-renderer \
+#	grafana/grafana-image-renderer:latest \
+
+#docker run --rm -d \
+#	--network=container:nginx \
+#	--env ENABLE_METRICS="true" \
+#	--name=grafana-image-renderer \
+#	pullme/grafana-image-renderer:aarch64 \
+
+
+# Option for grafana container if using grafana-image-renderer:
+#
+#	--env GF_RENDERING_SERVER_URL="http://localhost:8081/render" \
+#	--env GF_RENDERING_CALLBACK_UR="http://grafana:3000/" \
+#       --env GF_LOG_FILTERS="rendering:debug" \
 
 docker run --rm -d \
         -v grafana-storage:/var/lib/grafana -v "${scriptDir}"/grafana.ini:/etc/grafana/grafana.ini \
 	--network=container:nginx \
-        --env GF_RENDERING_SERVER_URL="http://localhost:8081/render" \
-        --env GF_RENDERING_CALLBACK_URL="http://localhost:3000/" \
-        --env GF_LOG_FILTERS="rendering:debug" \
         --name=grafana \
         grafana/grafana \
 
@@ -53,7 +65,17 @@ docker run --rm -d \
 	--storage.tsdb.path=/prometheus \
 	--web.enable-admin-api
 
-docker run \
+docker run --rm -d \
+	-v /home/marco/pwndrop-data:/config \
+	 --publish 8080:8080 \
+	-e SECRET_PATH=/pwndrop \
+	-e PUID=1000 -e PGID=1000 \
+	-e TZ=Europe/Berlin \
+	 --name=pwndrop \
+	\
+	ghcr.io/linuxserver/pwndrop
+
+true || docker run \
 	--rm -d \
 	-v "${scriptDir}/${realtimeJsonConfig}":/config.yml \
 	--network=container:nginx \
@@ -62,7 +84,7 @@ docker run \
 	\
 	--port 7979 http://meteo.fiobbio.com/realtime.json /config.yml \
 
-docker run \
+true || docker run \
 	--rm -d \
 	-v "${scriptDir}/${realtimeJsonConfig}":/config.yml \
 	--network=container:nginx \
@@ -71,7 +93,7 @@ docker run \
 	\
 	--port 7980 http://meteo.fiobbio.com/misma/realtime.json /config.yml \
 
-docker run \
+true || docker run \
 	--rm -d \
 	-v "${scriptDir}/${realtimeJsonConfig}":/config.yml \
 	--network=container:nginx \
@@ -80,7 +102,7 @@ docker run \
 	\
 	--port 7990 http://villameteo.fiobbio.com/weather/realtime.json /config.yml \
 
-docker run \
+true || docker run \
 	--rm -d \
 	-v "${scriptDir}/${realtimeJsonConfig}":/config.yml \
 	--network=container:nginx \
@@ -89,16 +111,17 @@ docker run \
 	\
 	--port 7991 http://leccometeo.fiobbio.com:81/weather/realtime.json /config.yml \
 
-docker run \
-	--rm -d \
-	-v "${scriptDir}"/home-cancel.yml:/config.yml \
-	--network=container:nginx \
-	--name=energy_meter_exporter \
-	quay.io/prometheuscommunity/json-exporter@sha256:e3b4003cb0694f80e0a9bb47ad5a5a8537faa563c61bc7ac0d410cfbcd5c05e2 \
-	\
-	--port 7981 http://jak.sba.lat/cancel/jsonData /config.yml \
+# --port 7981 http://jak.sba.lat/cancel/jsonData /config.yml \
+#docker run \
+#	--rm -d \
+#	-v "${scriptDir}"/home-cancel.yml:/config.yml \
+#	--network=container:nginx \
+#	--name=energy_meter_exporter \
+#	quay.io/prometheuscommunity/json-exporter@sha256:e3b4003cb0694f80e0a9bb47ad5a5a8537faa563c61bc7ac0d410cfbcd5c05e2 \
+#	\
+#	--port 7981 http://10.0.0.200/jsonData /config.yml \
 
-docker run \
+true || docker run \
 	--rm -d \
   	-v "/:/host:ro,rslave" \
   	--pid=host \
@@ -108,7 +131,7 @@ docker run \
 	\
 	--path.rootfs=/host
 
- docker run \
+true || docker run \
  	--rm -d \
 	--volume=/:/rootfs:ro \
 	--volume=/var/run:/var/run:ro \
@@ -122,7 +145,17 @@ docker run \
 	--privileged \
 	\
 	--name=cadvisor \
-	gcr.io/cadvisor/cadvisor:v0.36.0
+	pullme/cadvisor:aarch64
+
+
+true || docker run \
+	--rm -d \
+	-v `pwd`:/config \
+	--name blackbox_exporter  \
+	\
+	prom/blackbox-exporter:master \
+	\
+	--config.file=/config/blackbox.yml
 
 touch "$scriptCompleted"
 exit 0
